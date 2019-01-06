7464d61b2468b3072c7016bb3157766fc1eddd53
Coshyak <florian.hergenroeder@gmail.com>
2019-01-06 15:50:08 +0100

    Servicepro page

---
+++ crm/servicepro.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>andisos</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Search.css">
+    <link rel="stylesheet" href="assets/css/Sidebar-Menu.css">
+    <link rel="stylesheet" href="assets/css/Sidebar-Menu.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+<div id="wrapper">
+<?php
+include 'navigation.php';
+?>
+</div>
+
+</body>
+
+</html>
