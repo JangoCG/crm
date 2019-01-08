88899101edd7de0fd990dd1591608a011f47cd05
Cengiz <cengiz.guertusgil@gmail.com>
2019-01-08 22:06:59 +0100

    Accounts anlegen weiter entwickelt

--- crm/sales/accounts-anlegen.php
+++ crm/sales/accounts-anlegen.php
@@ -14,6 +14,15 @@ $vorname ="";
 $vornameError ="";
 $nachname ="";
 $nachnameError ="";
+$firma="";
+$firmaError="";
+$plz="";
+$plzError="";
+$land="";
+$strasse="";
+$strasseError="";
+$stadt="";
+$stadtError="";
 
 
 
@@ -47,13 +56,89 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         $nachname = trim($_POST["nachname"]);
     }
 
+    //Firma
+    if (empty(trim($_POST["firma"]))) {
+        $firmaError = "Bitte geben Sie einen firman ein";
+
+    } elseif (!(ctype_alpha($_POST["firma"]))) { //ctype_alpha prüft ob nur buchstaben vorhanden sind
+        $firmaError = "Bitte verwenden Sie nur Buchstaben für den firman";
+    } elseif (strlen(trim($_POST["firma"])) < 2) {
+        $firmaError = "Der firma muss 2 Buchstaben oder mehr enthalten";
+    } elseif (strlen(trim($_POST["firma"])) > 25) {
+        $firmaError = "Der firma muss 25 Buchstaben oder weniger enthalten";
+    } else {
+        $firma = trim($_POST["firma"]);
+    }
+
+    //PLZ
+    if (empty(trim($_POST["plz"]))) {
+        $plzError = "Bitte geben Sie einen plzn ein";
+
+    } elseif (!(ctype_digit ($_POST["plz"]))) { //ctype_alpha prüft ob nur aus zahlen besteht
+        $plzError = "Bitte verwenden Sie nur Buchstaben für den plzn";
+    } elseif (strlen(trim($_POST["plz"])) < 5) {
+        $plzError = "Der plz muss 2 Buchstaben oder mehr enthalten";
+    } elseif (strlen(trim($_POST["plz"])) > 5) {
+        $plzError = "Der plz muss 25 Buchstaben oder weniger enthalten";
+    } else {
+        $plz = trim($_POST["plz"]);
+    }
+
+
+    //Land
+
+    if (empty(trim($_POST["land"]))) {
+        $landError = "Bitte geben Sie einen landn ein";
+
+    } elseif (!(ctype_alpha($_POST["land"]))) { //ctype_alpha prüft ob nur buchstaben vorhanden sind
+        $landError = "Bitte verwenden Sie nur Buchstaben für den landn";
+    } elseif (strlen(trim($_POST["land"])) < 2) {
+        $landError = "Der land muss 2 Buchstaben oder mehr enthalten";
+    } elseif (strlen(trim($_POST["land"])) > 25) {
+        $landError = "Der land muss 25 Buchstaben oder weniger enthalten";
+    } else {
+        $land = trim($_POST["land"]);
+    }
+
+    //Straße
+
+    if (empty(trim($_POST["strasse"]))) {
+        $strasseError = "Bitte geben Sie einen strassen ein";
+
+    } elseif (!(ctype_alpha($_POST["strasse"]))) { //ctype_alpha prüft ob nur buchstaben vorhanden sind
+        $strasseError = "Bitte verwenden Sie nur Buchstaben für den strassen";
+    } elseif (strlen(trim($_POST["strasse"])) < 2) {
+        $strasseError = "Der strasse muss 2 Buchstaben oder mehr enthalten";
+    } elseif (strlen(trim($_POST["strasse"])) > 25) {
+        $strasseError = "Der strasse muss 25 Buchstaben oder weniger enthalten";
+    } else {
+        $strasse = trim($_POST["strasse"]);
+    }
+
+    //Stadt
+
+    if (empty(trim($_POST["stadt"]))) {
+        $stadtError = "Bitte geben Sie einen stadtn ein";
+
+    } elseif (!(ctype_alpha($_POST["stadt"]))) { //ctype_alpha prüft ob nur buchstaben vorhanden sind
+        $stadtError = "Bitte verwenden Sie nur Buchstaben für den stadtn";
+    } elseif (strlen(trim($_POST["stadt"])) < 2) {
+        $stadtError = "Der stadt muss 2 Buchstaben oder mehr enthalten";
+    } elseif (strlen(trim($_POST["stadt"])) > 25) {
+        $stadtError = "Der stadt muss 25 Buchstaben oder weniger enthalten";
+    } else {
+        $stadt = trim($_POST["stadt"]);
+    }
+
+
+
 
     //Vordem Insert überprüfen ob Fehler vorhanden sind
-    if(empty($vornameError) && empty($nachnameError)) {
+    if(empty($vornameError) && empty($nachnameError) && empty($firmaError) && empty($plzError) && empty($landError) && empty($strasseError) && empty($stadtError)) {
 
         //SQL Statement Variable übergeben
-        $sqlStatement = "INSERT INTO test (Vorname) 
-                     VALUES ('$vorname')";
+        $sqlStatement = "INSERT INTO test (Vorname, Nachname,Firma, PLZ, Land, Strasse) 
+                     VALUES ('$vorname','$nachname','$firma', '$plz', '$land', '$strasse')";
 
         //SQL Insert durchführen mit mysqli query
         if(mysqli_query($connection, $sqlStatement)) {
@@ -120,12 +205,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                     </div>
                     <div>
                         <label style="width:109.6px;">Nachname</label>
-                        <input type="text" name="nachname" value="<?php echo $Nachname; ?>" class="ml-2" style="background-color:#ffffff;">
+                        <input type="text" name="nachname" value="<?php echo $nachname; ?>" class="ml-2" style="background-color:#ffffff;">
                         <span class="help-block"><?php echo $nachnameError; ?></span>
                     </div>
                     <div>
                         <label style="width:109.6px;">Geburtsdatum</label>
-                        <input type="text" name="geburtsdatum" value="<?php echo $geburstdatum; ?>"class="ml-2">
+                        <input type="date" name="geburtsdatum" value="<?php echo $geburstdatum; ?>" class="ml-2">
                         <span class="help-block"><?php echo $geburstdatumError; ?></span>
                     </div>
                     <div>
@@ -135,10 +220,27 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                 </div>
                 <div class="col">
                     <h1>&nbsp;</h1>
-                    <div><label style="width:109.6px;">Straße</label><input type="text" class="ml-2"></div>
-                    <div><label style="width:109.6px;">PLZ</label><input type="text" class="ml-2"></div>
-                    <div><label style="width:109.6px;">Ort</label><input type="text" class="ml-2"></div>
-                    <div><label style="width:109.6px;">Land</label><input type="text" class="ml-2"><button class="btn btn-primary such-button" type="submit">Account anlegen</button></div>
+                    <div>
+                        <label style="width:109.6px;">Straße</label>
+                        <input type="text" name="strasse" value="<?php echo $strasse; ?>" class="ml-2">
+                        <span class="help-block"><?php echo $strasseError; ?></span>
+                    </div>
+                    <div>
+                        <label style="width:109.6px;">PLZ</label>
+                        <input type="text" name="plz" value="<?php echo $plz; ?>" class="ml-2">
+                        <span class="help-block"><?php echo $plzError; ?></span>
+                    </div>
+                    <div>
+                        <label style="width:109.6px;">Stadt</label>
+                        <input type="text" name="stadt" value="<?php echo $ort;?>" class="ml-2">
+                        <span class="help-block"><?php echo $ortError; ?></span>
+                    </div>
+                    <div>
+                        <label style="width:109.6px;">Land</label>
+                        <input type="text" name="land" value="<?php echo $land; ?>" class="ml-2">
+                        <span class="help-block"><?php echo $landError; ?></span>
+                        <button class="btn btn-primary such-button" type="submit">Account anlegen</button>
+                    </div>
                     </form>
                 </div>
             </div>
