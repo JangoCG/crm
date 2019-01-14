484b4305c084ce50c1e05597d5ba85a750da84c9
Hakanc16 <Hakan-Cokkisku@live.de>
2019-01-14 15:48:45 +0100

    Merge branch 'master' of https://github.com/JangoCG/crm
    Letzte Änderungen von Hakan Cokkisku:

---
+++ crm/Marketing/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produkt anlegen</title>
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/Navigation-with-Search.css">
+    <link rel="stylesheet" href="../assets/css/Sidebar-Menu.css">
+    <link rel="stylesheet" href="../assets/css/Sidebar-Menu.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
+</head>
+
+
+<body>
+<div id="wrapper">
+    <div id="sidebar-wrapper" style="background-color:#37434d;">
+        <h1><a href="index.php">Marketingpro</a></h1>
+        <div class="mt-5">
+            <div class="dropdown amk-border"><a class="btn btn-primary dropdown-toggle kein-rahmen" data-toggle="dropdown" aria-expanded="false" role="button" href="#">Accounts & Produkte</a>
+                <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="ProduktAnlegen.php">Produkt anlegen</a><a class="dropdown-item" role="presentation" href="ProduktSuchen.php">Produkt suchen</a><a class="dropdown-item" role="presentation" href="PrivatAccountanlegen.php">Privat-Account anlegen</a><a class="dropdown-item" role="presentation" href="PrivatAccountsuchen.php">Privat-Account suchen</a></div>
+            </div>
+            <div class="dropdown"><button class="btn btn-primary dropdown-toggle kein-rahmen" data-toggle="dropdown" aria-expanded="false" type="button" style="width:248px;">Marketing</button>
+                <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">Leads anlegen</a><a class="dropdown-item" role="presentation" href="#">Leads suchen</a><a class="dropdown-item" role="presentation" href="KampagneAnlegen.php">Kampagne anlegen</a><a class="dropdown-item" role="presentation" href="#">Kampagne suchen</a><a class="dropdown-item" role="presentation" href="#">Marketingplan anlegen</a><a class="dropdown-item" role="presentation" href="#">Marketingplan suchen</a><a class="dropdown-item" role="presentation" href="Dateisuche.php">Datei suchen</a></div>
+            </div>
+        </div>
+
+
+        <div></div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col text-right" style="background-color:#37434d;"><input type="search" placeholder="Suchbegriff eingeben" id="grossesFeld"><button class="btn btn-primary ml-2 mt-1 mb-1" type="button">Button</button></div>
+        </div>
+    </div>
+
+    <div class="row">
+        &emsp;&emsp;<div class="col">
+            <br><h3>Wilkommen in ihrem MarketingPro-Bereich.</h3>
+
+            <p><b>Wetter-Widget</p>
+
+            <iframe
+                    src="https://www.wetter.de/deutschland/wetter-fulda-18221348.html"
+                    width="90%"
+                    height="400"
+                    name="SELFHTML_in_a_box">
+
+                <p>
+                    <a href="https://wiki.selfhtml.org/wiki/Startseite">SELFHTML</a>
+                </p>
+            </iframe>
+        </div>
+
+            <div class="col">
+                </b><br><br><br><b><p>Golem-Widget</p>
+                <iframe
+                        src="https://www.golem.de/"
+                        width="90%"
+                        height="400"
+                        name="SELFHTML_in_a_box">
+
+                    <p>
+                        <a href="https://wiki.selfhtml.org/wiki/Startseite">SELFHTML</a>
+                    </p>
+                </iframe>
+            </div>
+
+    </div>
+
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="../assets/js/Sidebar-Menu.js"></script>
+</body>
+
+</html>
