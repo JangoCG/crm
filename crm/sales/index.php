73d70b25dde97d56006b28533e5c682714ae7698
Cengiz.Guertusgil <cengiz.gurtusgil@gmail.com>
2019-01-25 22:21:54 +0100

    Account Suche hinzugefügt.

--- crm/sales/index.php
+++ crm/sales/index.php
@@ -43,7 +43,40 @@
             <div class="col"></div>
         </div>
     </div>
-</div>
+
+    <div class="row">
+        &emsp;&emsp;<div class="col">
+            <br><h3>Wilkommen in ihrem MarketingPro-Bereich.</h3>
+
+            <p><b>Wetter-Widget</p>
+
+            <iframe
+                    src="https://www.wetter.de/deutschland/wetter-fulda-18221348.html"
+                    width="90%"
+                    height="400"
+                    name="SELFHTML_in_a_box">
+
+                <p>
+                    <a href="https://wiki.selfhtml.org/wiki/Startseite">SELFHTML</a>
+                </p>
+            </iframe>
+        </div>
+
+        <div class="col">
+            </b><br><br><br><b><p>Golem-Widget</p>
+                <iframe
+                        src="https://www.golem.de/"
+                        width="90%"
+                        height="400"
+                        name="SELFHTML_in_a_box">
+
+                    <p>
+                        <a href="https://wiki.selfhtml.org/wiki/Startseite">SELFHTML</a>
+                    </p>
+                </iframe>
+        </div>
+
+    </div>
 <script src="../assets/js/jquery.min.js"></script>
 <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
 <script src="../assets/js/Sidebar-Menu.js"></script>
